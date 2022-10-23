c757f8f5783fa57bdaf7278e6471df150c841b6c
kahootali <kahoot.ali@gmail.com>
2022-10-23 12:06:24 +0500

    add step to deploy app using jenkins

--- hello-world-nodejs-docker/server.js
+++ hello-world-nodejs-docker/server.js
@@ -11,6 +11,9 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
+app.get('/batch', (req, res) => {
+  res.send('10\n');
+});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
